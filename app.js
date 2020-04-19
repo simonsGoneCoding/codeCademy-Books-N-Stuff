59af595c79e362b758bd68989373b85230d1be7f
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-04-19 15:20:09 +0100

    Created Media class and two sub - classes.

---
+++ app.js
@@ -0,0 +1,63 @@
+class Media {
+  constructor(title) {
+    this._title = title;
+    this._isCheckedOut = false;
+    this._ratings = [];
+  }
+
+  // --------- getters:
+  get title() {
+    return this._title;
+  }
+  get isCheckedOut() {
+    return this._isCheckedOut;
+  }
+  get ratings() {
+    return this._ratings;
+  }
+
+  //----------- setters:
+  set isCheckedOut(value) {
+    this._isCheckedOut = value;
+  }
+
+  //----------- methods:
+  toggleCheckOutStatus() {
+    return (this._isCheckedOut = !this._isCheckedOut);
+  }
+
+  getAverageRating() {
+    let ratingsSum = this.ratings.reduce((a, b) => a + b);
+    const lengthOfArray = this.ratings.length;
+    return ratingsSum / lengthOfArray;
+  }
+
+  addRating(rating) {
+    return this.ratings.push(rating);
+  }
+}
+
+class Book extends Media {
+  constructor(title, author, pages) {
+    super(title);
+    this._author = author;
+    this._pages = pages;
+  }
+
+  //getters:
+  get author() {
+    return this._author;
+  }
+
+  get pages() {
+    return this._pages;
+  }
+}
+
+class Movie extends Media {
+  constructor(title, director, runTime) {
+    super(title);
+    this._director = director;
+    this._runTime = runTime;
+  }
+}
